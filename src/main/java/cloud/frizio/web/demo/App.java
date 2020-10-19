ffae46e07de05828a9711ccc4c7c0af2ac0a83da
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-10-19 18:26:19 +0200

    first commit

---
+++ src/main/java/cloud/frizio/web/demo/App.java
@@ -0,0 +1,11 @@
+package cloud.frizio.web.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Demo Hibernate" );
+    }
+}
