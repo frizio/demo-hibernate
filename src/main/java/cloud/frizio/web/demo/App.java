3dd81eedb29e08458e0d23b7523f20800951aa4d
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-10-29 16:03:36 +0100

    Setting up JDBC connection

--- src/main/java/cloud/frizio/web/demo/App.java
+++ src/main/java/cloud/frizio/web/demo/App.java
@@ -1,14 +1,27 @@
 package cloud.frizio.web.demo;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class App {
     public static void main( String[] args ) {
+
         System.out.println( "Demo Hibernate" );
 
-        
+        String user = "wildfly_user";
+        String password = "password1;";
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+          System.out.println("Connecting to db: " + url);
+          Class.forName(driver);
+          Connection connection = DriverManager.getConnection(url, user, password);
+          System.out.println("Successful Connection.");
+          connection.close();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
 
     }
 }
